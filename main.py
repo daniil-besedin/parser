40903f8b5eae4c2b696990c75496a27f0c962879
Besedin Daniil <55641460+daniil-besedin@users.noreply.github.com>
2020-10-27 23:31:45 +0300

    download books from tululu.org

--- main.py
+++ main.py
@@ -1,17 +1,22 @@
 import requests
+import os
 
 
 def download_book(url, file_name):
-    response = requests.get(url)
+    response = requests.get(url, allow_redirects=False)
     response.raise_for_status()
-    with open(file_name, 'w', encoding='utf-8') as file:
-        file.write(response.text)
+    if response.status_code == 200:
+        with open('folder_0/' + file_name, 'w', encoding='utf-8') as file:
+            file.write(response.text)
 
 
 if __name__ == '__main__':
+    path = 'folder_0'
+    os.makedirs(path, exist_ok=True)
     try:
-        url = 'https://tululu.org/txt.php?id=32168'
-        file_name = 'book.txt'
-        download_book(url, file_name)
+        for id in range(1, 11):
+            url = 'https://tululu.org/txt.php?id={id}'.format(id=id)
+            file_name = 'id{id}.txt'.format(id=id)
+            download_book(url, file_name)
     except requests.exceptions.MissingSchema as err:
         print('Invalid link to book')
