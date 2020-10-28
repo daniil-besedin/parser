46c17855e81802daf71d86f2cf1103de3e234b0c
Besedin Daniil <55641460+daniil-besedin@users.noreply.github.com>
2020-10-28 07:38:34 +0300

    download books with titles

--- main.py
+++ main.py
@@ -1,22 +1,36 @@
 import requests
 import os
+from bs4 import BeautifulSoup
+from pathvalidate import sanitize_filename
 
 
-def download_book(url, file_name):
+def download_book(url, filename, folder='books'):
+    os.makedirs(folder, exist_ok=True)
     response = requests.get(url, allow_redirects=False)
     response.raise_for_status()
+    filepath = os.path.join(folder, filename)
     if response.status_code == 200:
-        with open('folder_0/' + file_name, 'w', encoding='utf-8') as file:
+        with open(filepath, 'w', encoding='utf-8') as file:
             file.write(response.text)
+        return filepath
+    return None
+
+
+def get_file_name(id):
+    cover_url = 'https://tululu.org/b{id}/'.format(id=id)
+    response = requests.get(cover_url)
+    response.raise_for_status()
+    soup = BeautifulSoup(response.text, 'lxml')
+    title = soup.find('h1').text.split('::')[0].strip()
+    return sanitize_filename('{id}. {title}.txt'.format(id=id, title=title))
 
 
 if __name__ == '__main__':
-    path = 'folder_0'
-    os.makedirs(path, exist_ok=True)
     try:
         for id in range(1, 11):
             url = 'https://tululu.org/txt.php?id={id}'.format(id=id)
-            file_name = 'id{id}.txt'.format(id=id)
-            download_book(url, file_name)
+            filename = get_file_name(id)
+            filepath = download_book(url, filename)
+            print(filepath)
     except requests.exceptions.MissingSchema as err:
         print('Invalid link to book')
