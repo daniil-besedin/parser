e3c20edc3c50a5afcd7c16abf5199fd03a9f0a49
Besedin Daniil <55641460+daniil-besedin@users.noreply.github.com>
2020-11-09 12:17:22 +0300

    removed certificate verification and added try except

--- main.py
+++ main.py
@@ -5,12 +5,13 @@ from pathvalidate import sanitize_filename
 from urllib.parse import urljoin
 import json
 import argparse
+from time import sleep
 
 
 def download_book(url, filename, folder='books', main_folder=''):
     folder = os.path.join(main_folder, folder)
     os.makedirs(folder, exist_ok=True)
-    response = requests.get(url, allow_redirects=False)
+    response = requests.get(url, allow_redirects=False, verify=False)
     response.raise_for_status()
     book_path = os.path.join(folder, filename)
 
@@ -24,7 +25,7 @@ def download_book(url, filename, folder='books', main_folder=''):
 def download_img(url, filename, folder='images', main_folder=''):
     folder = os.path.join(main_folder, folder)
     os.makedirs(folder, exist_ok=True)
-    response = requests.get(url, allow_redirects=False)
+    response = requests.get(url, allow_redirects=False, verify=False)
     response.raise_for_status()
     img_path = os.path.join(folder, filename)
 
@@ -36,7 +37,7 @@ def download_img(url, filename, folder='images', main_folder=''):
 
 
 def get_content(id, url, book_description, base_url, main_folder='', skip_imgs=False, skip_txt=False):
-    response = requests.get(url, allow_redirects=False)
+    response = requests.get(url, allow_redirects=False, verify=False)
     response.raise_for_status()
     if response.status_code != 200:
         return
@@ -62,7 +63,7 @@ def get_content(id, url, book_description, base_url, main_folder='', skip_imgs=F
     img_url = urljoin(url, img)
     if not skip_imgs:
         os.makedirs(main_folder, exist_ok=True)
-        img_path = download_img(img_url, img_name, main_folder)
+        img_path = download_img(img_url, img_name, main_folder=main_folder)
         book_description['img_src'] = img_path
 
     comments_selector = 'div.texts'
@@ -92,53 +93,65 @@ def create_parser():
 
 
 def main():
-    try:
-        parser = create_parser()
-        args = parser.parse_args()
+    requests.packages.urllib3.disable_warnings()
+    parser = create_parser()
+    args = parser.parse_args()
 
-        main_folder = args.dest_folder
-        skip_imgs = args.skip_imgs
-        skip_txt = args.skip_txt
-        json_path = args.json_path
-        base_url = 'https://tululu.org'
-        template_genre_url = 'https://tululu.org/l55/{page}'
-        start_page = args.start_page
-        end_page = args.end_page
-        book_descriptions = []
+    main_folder = args.dest_folder
+    skip_imgs = args.skip_imgs
+    skip_txt = args.skip_txt
+    json_path = args.json_path
+    base_url = 'https://tululu.org'
+    template_genre_url = 'https://tululu.org/l55/{page}'
+    start_page = args.start_page
+    end_page = args.end_page
+    book_descriptions = []
 
-        for page in range(start_page, end_page):
-            genre_page_url = template_genre_url.format(page=page)
-            response = requests.get(genre_page_url, allow_redirects=False)
+    for page in range(start_page, end_page):
+        genre_page_url = template_genre_url.format(page=page)
+
+        try:
+            response = requests.get(genre_page_url, allow_redirects=False, verify=False)
             response.raise_for_status()
-            if response.status_code != 200:
-                print('Страниы по адресу {} не существует'.format(genre_page_url))
-                continue
-            soup = BeautifulSoup(response.text, 'lxml')
-            books_selector = 'table.d_book'
-            books = soup.select(books_selector)
+        except requests.exceptions.HTTPError as err:
+            print(err)
+            continue
+        except requests.exceptions.ConnectionError as err:
+            print(err)
+            sleep(5)
+            continue
 
-            for book in books:
-                book_description = {}
+        if response.status_code != 200:
+            print('Страниы по адресу {} не существует'.format(genre_page_url))
+            continue
+        soup = BeautifulSoup(response.text, 'lxml')
+        books_selector = 'table.d_book'
+        books = soup.select(books_selector)
 
-                url_selector = 'a'
-                url = book.select_one(url_selector)['href']
-                id = url.strip('/b')
-                book_url = urljoin(base_url, url)
+        for book in books:
+            book_description = {}
+
+            url_selector = 'a'
+            url = book.select_one(url_selector)['href']
+            id = url.strip('/b')
+            book_url = urljoin(base_url, url)
+
+            try:
                 get_content(id, book_url, book_description, base_url, main_folder=main_folder, skip_imgs=skip_imgs,
-                            skip_txt=skip_txt)
+                        skip_txt=skip_txt)
+            except requests.exceptions.HTTPError as err:
+                print(err)
+                continue
+            except requests.exceptions.ConnectionError as err:
+                print(err)
+                sleep(5)
+                continue
 
-                book_descriptions.append(book_description)
-                print(book_url)
+            book_descriptions.append(book_description)
+            print(book_url)
 
-        with open(json_path, 'w', encoding='utf8') as file:
-            json.dump(book_descriptions, file, ensure_ascii=False)
-
-    except requests.exceptions.MissingSchema as err:
-        print(err)
-    except requests.exceptions.HTTPError as err:
-        print(err)
-    except ValueError as err:
-        print('You did not enter a number')
+    with open(json_path, 'w', encoding='utf8') as file:
+        json.dump(book_descriptions, file, ensure_ascii=False)
 
 
 if __name__ == '__main__':
